6e4c2ff8cbb50d40b419974112c5fb8c3e789ce9
Marie Gillier <mgillier@icij.org>
2023-03-09 14:31:14 +0100

    fix: run linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,43 +2,41 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-    env: {
-        jest: true,
-        browser: true,
-        es2021: true
-    },
-    extends: [
-        "standard",
-        "plugin:vue/recommended",
-        // prettier should be last to override all formatting rules from eslint
-        "prettier"
+  env: {
+    jest: true,
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:vue/recommended',
+    // prettier should be last to override all formatting rules from eslint
+    'prettier'
+  ],
+  overrides: [],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
+  },
+  plugins: ['prettier', 'vue', 'import'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none',
+        printWidth: 120
+      }
     ],
-    overrides: [],
-    parserOptions: {
-        parser: "@babel/eslint-parser",
-        requireConfigFile: false,
-    },
-    plugins: [
-        "prettier",
-        "vue",
-        "import"
-    ],
-    rules: {
-        "prettier/prettier": ["error",
-            {
-                singleQuote: true,
-                semi: false,
-                trailingComma: "none",
-                printWidth: 120
-            }],
-        "import/no-webpack-loader-syntax": "off",
-        "no-useless-escape": "off",
-        "lines-between-class-members": "off",
-        "template-curly-spacing": "off",
-        "vue/custom-event-name-casing": "off",
-        "vue/multi-word-component-names": "off",
-        "import/no-extraneous-dependencies": "off",
-        "vue/require-default-prop": "off",
-        "vue/no-v-html": "off"
-    }
+    'import/no-webpack-loader-syntax': 'off',
+    'no-useless-escape': 'off',
+    'lines-between-class-members': 'off',
+    'template-curly-spacing': 'off',
+    'vue/custom-event-name-casing': 'off',
+    'vue/multi-word-component-names': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/no-v-html': 'off'
+  }
 }
